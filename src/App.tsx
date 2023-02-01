1c4d8e90936eeb8b8b080a7b6eace1af8f4a591b
Restyled.io <commits@restyled.io>
2023-02-01 17:34:07 +0000

    Restyled by whitespace

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      
+
       <ul ref={ref}>
       <svg id="progress" width="100" height="100" viewBox="0 0 100 100">
         <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
